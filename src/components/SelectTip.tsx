fba40bb1823900758d81270f102e1528e056d6f3
Thomas <thomas.bogle@outlook.com>
2024-11-30 23:16:23 +0000

    fix custom tip not updating. First css stylings for Result component

--- src/components/SelectTip.tsx
+++ src/components/SelectTip.tsx
@@ -1,7 +1,15 @@
 import Tip from "./Tip"
 import '../styles/selecttip.css'
+import BillContext from "../utils/BillContext"
+import { ChangeEvent, useContext } from "react"
 
 const SelectTip = () => {
+    const context = useContext(BillContext)
+    const { setTip } = context
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTip(e.target.value)
+      }
 
     return (
         <>
@@ -13,7 +21,7 @@ const SelectTip = () => {
                 <Tip value={"15%"} />
                 <Tip value={"25%"} />
                 <Tip value={"50%"} />
-                <input className="custom-input" placeholder="Custom" type="datetime" maxLength={2}></input>
+                <input className="custom-input" placeholder="Custom" type="datetime" maxLength={2} onChange={handleChange}></input>
             </span>
         </div>
         </>
