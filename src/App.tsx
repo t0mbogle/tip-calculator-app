687dad0b063e2e6ba67f98098da5b4e8b3d50845
Thomas <thomas.bogle@outlook.com>
2024-12-16 19:32:40 +0000

    add input errors to Bill amount

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './styles/App.css'
 import SelectTip from './components/SelectTip'
 import { useState } from 'react'
 import BillContext from './utils/BillContext'
-import BillValue from './components/BillValue'
+import BillValue from './components/BillAmount'
 import People from './components/People'
 import Result from './components/Result'
 
 function App() {
   const [amount, setAmount] = useState<string>('')
-  const [tip, setTip] = useState<string | null>(null);
+  const [tip, setTip] = useState<string>('');
   const [people, setPeople] = useState<string>('');
 
   return (
